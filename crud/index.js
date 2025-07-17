ba997c1fd9b11382eb88769f38220d556a025f1e
Gautam Parmar <gautamparmar03214@gmail.com>
2025-07-17 11:07:31 +0530

    hello

---
+++ crud/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const server = express()
+const mysql = require('mysql')
+
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'student'
+})
+connection.connect((error) => {
+    if(error) throw error
+    console.log("connection success");
+    
+})
+
+// console.log('hello');
+server.set('view engine','ejs')
+
+server.get('/' , (req , res ) => {
+
+    var qry = `SELECT * FROM class`
+
+    connection.query(qry , (error , data) => {
+        if(error) throw error
+        res.render('datas',{data})
+    })
+
+})
+
+server.get('/createData',(req , res) => {
+    const data = req.query
+    console.log(data);
+    const {name , password} = req.query
+
+    var qry = `INSERT INTO class (name , password) values ('${name}' , '${password}')`
+
+    connection.query(qry , (error) => {
+        if(error) throw error
+        console.log("data rnter success"); 
+    })
+    res.redirect('/')
+})
+
+server.listen(4444)
